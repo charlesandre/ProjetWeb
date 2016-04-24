1fa2fd67cafc279d27b985c657cb884d32ab0c74
Alex <Alex@MacBook-Air-de-Alexandre.local>
2016-04-24 18:00:24 +0200

    Ajout d'une page reglages

--- MyAccount.php
+++ MyAccount.php
@@ -13,49 +13,6 @@ if(isset($_GET['id']) AND $_GET['id']>0)
 	$login = $row[1];
 	$email = $row[2];
 	$pw = $row[3];
-
-if (isset($_POST['formmodif']))
-	{
-		if(!empty($_POST['log']) AND !empty($_POST['email']) AND !empty($_POST['pass']) AND !empty($_POST['pass2']) AND !empty($_POST['pass3']))
-		{
-			$loginform = htmlspecialchars($_POST['log']);
-			$emailform = htmlspecialchars($_POST['email']);
-			$pw1 = htmlspecialchars($_POST['pass']);
-			$pw2 = htmlspecialchars($_POST['pass2']);
-			$pw3 = htmlspecialchars($_POST['pass3']);
-
-			if($pw2 == $pw3)
-			{
-				if($pw1 == $pw){
-
-				$result = mysql_query("UPDATE Users SET Login = '$loginform', Email = '$emailform', Password = '$pw3' WHERE ID = '$getid'");
-				if($result)
-				{
-					header('Location: Home.php?id=<?php echo $getid ?>');
-					
-				}
-				}
-				else 
-				{
-					$erreur = "L'ancien mot de passe ne correspond pas";
-				}
-				
-			}
-			else
-			{
-				$erreur = "Les mots de passe sont différents";
-			}
-		}	
-		else
-		{
-			$erreur = "All fields must be completed";		
-		}
-		
-	}
-
-	
- 
-
 ?>
 <html>
 	<head>
@@ -85,79 +42,12 @@ if (isset($_POST['formmodif']))
 			<h1> Mon compte </h1>
 			<h2> Bienvenue <?php echo $login; ?> Comment allez vous ? </h2>
 
-
-			<div id ="modificationinfo">
-				<form method="post" action ="" align ="center">
-				<p id="texteInscription">Modifier mes informations</p>
-				<div>
-				<table>
-					<tr>
-						<td>
-							<label for "login"> Pseudo : </label>
-						</td>
-						<td>
-							<input id ="login" type="text" name="log" required="required" placeholder="<?php echo $login; ?>" class="caseConnexion"/>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for "email"> E-mail : </label>
-						</td>
-						<td>
-							<input id ="email" type="TEXT" name="email"  required="required" placeholder="<?php echo $email; ?>" class="caseConnexion" />
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for "pass">Ancien mot de passe </label>
-						</td>
-						<td>
-							<input id ="pass" type="password" name="pass"  required="required" placeholder="Ancien mot de passe" class="caseConnexion"/>
-						</td>
-					</tr>
-
-					<tr>
-						<td>
-							<label for "pass">Nouveau mot de passe </label>
-						</td>
-						<td>
-							<input id ="pass" type="password" name="pass2"  required="required" placeholder="Nouveau mot de passe" class="caseConnexion"/>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<label for "pass2"> Confirmer le nouveau mot de passe </label>
-						</td>
-						<td>
-							<input id ="pass2" type="password" name="pass3"  required="required" placeholder="Nouveau mot de passe" class="caseConnexion"/>
-						</td>
-					</tr>
-					<tr>
-					<td>
-						<input type = "submit" name="formmodif" value="Sauvegarder les modifications"> </input> 
-					</td>
-				</tr>
-
-				</table>
-
-
-					
-				</div>
-
-			
-
-			<?php
-			if(isset($erreur))
-			{
-				echo "<p id='erreurInscription'>".$erreur. "</p>";
-			}
-			?>
-
-					
-			</form>
-
-
+			<div id="divImageReglages">
+				<a href="Reglages.php?id=<?php echo $getid ?>" id="aReglages">
+					<img src="images/reglages.png" id="imageReglages"/>
+				</a>
 			</div>
+			
 
 		</div>
 
